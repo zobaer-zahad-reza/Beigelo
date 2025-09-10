aa571f40a0ee99357e92bb4c83ab69583065044e
zobaer-zahad-reza <zobaerzahad.git@gmail.com>
2025-09-10 14:08:41 +0600

    added daisyui

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import HeroVid0 from '../assets/HeroVid0.mp4';
+import React from "react";
+import HeroVid0 from "../assets/HeroVid0.mp4";
 
 function Hero() {
   return (
-    <div className='w-full h-[70vh]'>
-      <video 
-        className='w-full h-full object-cover'
-        src={HeroVid0} 
-        autoPlay 
-        loop 
-        muted 
+    <div className="w-full h-[70vh] ">
+      <video
+        className="w-full h-full object-cover rounded-lg"
+        src={HeroVid0}
+        autoPlay
+        loop
+        muted
         playsInline
       >
         Your browser does not support the video tag.
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {port:5173}
-})
+  plugins: [react(), tailwindcss()],
+  server: { port: 5173 },
+});
