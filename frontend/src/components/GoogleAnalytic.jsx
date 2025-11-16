a77ad346995bdbf0cbc854dcbf59dd3b7609d2d6
zobaer-zahad-reza <zobaerzahad.git@gmail.com>
2025-11-16 15:02:24 +0600

    write on robots file and set more google analytic in react

---
+++ frontend/src/components/GoogleAnalytic.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+import ReactGA from "react-ga4";
+
+const GoogleAnalytic = () => {
+  useEffect(() => {
+    ReactGA.initialize("G-0BMMC8ZH2T"); // Replace with your actual Tracking ID
+    ReactGA.send("pageview");
+  }, []);
+
+  return null;
+};
+
+export default GoogleAnalytic;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,13 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import ShopContextProvider from "./context/ShopContext.jsx";
+import GoogleAnalytic from "./components/GoogleAnalytic.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <ShopContextProvider>
       <App />
+      <GoogleAnalytic />
     </ShopContextProvider>
   </BrowserRouter>
 );
