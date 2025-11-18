42221f4cc3f1ac35069759471689a0d623d83ddf
zobaer-zahad-reza <zobaerzahad.git@gmail.com>
2025-11-19 02:10:44 +0600

    added new hero video

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import HeroVid0 from "../assets/HeroVid4.mp4";
 
 function Hero() {
   return (
-    <div className="w-full md:h-[70vh]">
-      <video
-        className="w-full h-full object-cover rounded-lg"
-        src={HeroVid0}
-        autoPlay
-        loop
-        muted
-        playsInline
-      >
-        Your browser does not support the video tag.
-      </video>
+    <div className="relative w-full h-[50vh] md:h-[70vh] overflow-hidden bg-black rounded-md">
+      <iframe
+        className="absolute top-1/2 left-1/2 w-[177.77vh] min-w-full min-h-[56.25vw] -translate-x-1/2 -translate-y-1/2 pointer-events-none"
+        src="https://www.youtube.com/embed/L1JoUErVauw?autoplay=1&mute=1&loop=1&playlist=L1JoUErVauw&controls=0&showinfo=0&rel=0&iv_load_policy=3&modestbranding=1"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
+
+      <div className="absolute inset-0 bg-transparent"></div>
     </div>
   );
 }
