e65037746e473e8ce292ab5d164d8074f3b1183e
zobaer-zahad-reza <zobaerzahad.git@gmail.com>
2025-12-06 01:23:51 +0600

    checking something

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Title from "../components/Title";
-import { assets } from "../assets/assets";
 import NewsletterBox from "../components/NewsletterBox";
 import LocationMap from "../components/LocationMap";
 
@@ -13,7 +12,7 @@ const About = () => {
       <div className="my-10 flex flex-col md:flex-row gap-16">
         <img
           className="w-full md:max-w-[450px]"
-          src={assets.about_img}
+          src="https://i.postimg.cc/ZYM3PTg4/about-img-BAJy-TXw9.png"
           alt=""
         />
         <div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-600">
