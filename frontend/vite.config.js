d5181ebcb8a6e9f8fbe68426ea85cd3f3e341584
zobaer-zahad-reza <zobaerzahad.git@gmail.com>
2025-12-08 14:52:01 +0600

    optimizing about page

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,14 +3,29 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { fileURLToPath, URL } from "url";
 
-
 export default defineConfig({
   plugins: [react()],
   server: { port: 5173 },
   resolve: {
     alias: {
-
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  build: {
+    cssCodeSplit: true,
+
+    sourcemap: false,
+
+    chunkSizeWarningLimit: 500,
+
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
     },
   },
 });
